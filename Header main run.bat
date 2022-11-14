f548dc571ecc95b2f43ed440013bf457e79ccf20
Manoj.Ryali007 <manoj.ryali@ril.com>
2022-11-14 16:31:27 +0530

    update bat

--- Header main run.bat
+++ Header main run.bat
@@ -1,3 +1,3 @@
-cd  C:\Bond automation\Jio.com_git\bond_automation
+cd  C:\Bond automation\Jenkins-Git
 
 robot -d Results/Mobile_Results/Header_Results  Tests/Mobile_tests/Header_Tests/Header_main.robot
